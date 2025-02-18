d219b64ddef88fa59cdee7cd04f01f7d8a552ee9
lich0005 <lich0005@stud.kea.dk>
2025-02-18 13:42:28 +0100

    single recipe 1

---
+++ js/single_recipe.js
@@ -0,0 +1,100 @@
+const one_recipe = document.querySelector(".one_recipe");
+const recipeId = new URLSearchParams(window.location.search).get("id");
+const navBack = document.querySelector(".back");
+const popular = document.querySelector(".popular");
+
+fetch(`https://dummyjson.com/recipes/1`)
+  .then((response) => response.json())
+  .then((data) => showRecipe(data));
+
+function showRecipe(recipe) {
+  one_recipe.innerHTML = `
+    <section class="intro">
+    <div class="intro_text">
+      <h1>${recipe.name}</h1>
+      
+      
+      <figure>
+      <img src="${recipe.image}" alt="Image of ${recipe.name}"></img>
+      </figure>
+      
+
+      <h2>Difficulty: ${recipe.difficulty}</h2>
+      <div>
+      <h2>Ingredients</h2>
+      <ul>
+      ${recipe.ingredients.map((ingredient) => `<li>${ingredient}</li>`).join("")}
+      </ul>  
+      </div>    
+    </div>
+
+    
+    </section>
+
+    <section class="details">
+      <dl>
+        <dt>Prepping:</dt>
+        <dd>${recipe.prepTimeMinutes} min</dd>
+      </dl>
+
+      <dl>
+        <dt>Cooking:</dt>
+        <dd>${recipe.cookTimeMinutes} min</dd>
+      </dl>
+
+      <dl>
+        <i class="fa-light fa-plate-utensils"></i>
+        <dt>Servings:</dt>
+        <dd>${recipe.servings}<dd/>
+      </dl>
+
+      <dl>
+        <i class="fa-regular fa-star"></i>        
+        <dt>Rating:</dt>
+        <dd>${recipe.rating} / 5</dd>        
+      </dl>
+    </section>
+  
+    <section>
+      <h2>Instructions</h2>
+      <ol>
+      ${recipe.instructions.map((steps) => `<li>${steps}</li>`).join("")}
+      </ol>  
+    </section>
+  `;
+
+  navBack.innerHTML = `<button class=back_button>Go back</button>`;
+  document.querySelector(".back_button").addEventListener("click", () => {
+    window.history.back();
+  });
+}
+
+fetch(`https://dummyjson.com/recipes`)
+  .then((response) => response.json())
+  .then((data) => mostPopular(data));
+
+function mostPopular(recipes) {
+  const topRecipes = recipes.recipes
+    .filter((recipe) => ["Italian", "Mediterranean", "Greek"].includes(recipe.cuisine))
+    .sort((a, b) => b.rating - a.rating) // arrangere rækkefølgen af opskrifterne fra bedst rating først
+    .slice(0, 3); //så der kun er 3
+
+  popular.innerHTML = ` 
+    <h2>Most popular</h2>
+    <div class="popular-grid">
+      ${topRecipes
+        .map(
+          (recipe) => `
+        <article class="popular-item">
+          <a href="single_recipe.html?id=${recipe.id}">
+            <img src="${recipe.image}" alt="${recipe.name}">
+            <h3>${recipe.name}</h3>
+            <p>⭐Rating: ${recipe.rating} / 5</p>
+          </a>
+        </article>
+      `
+        )
+        .join("")}
+    </div>
+  `;
+}
